495c073bd85f578411fd762cb342baa71fdd1b50
ImpFeroci <brian@famgeerdink.nl>
2024-04-03 18:55:51 +0200

    updated 0.0001

--- main.py
+++ main.py
@@ -14,15 +14,24 @@ def load_game_function():
 def options_function():
   print("De knop 'Options' is ingedrukt!")
 
+# Maak een functie voor de knop "Exit"
+def exit_function():
+  window.destroy()
+
 # Maak knoppen
 new_game_button = tk.Button(text="New Game", command=new_game_function)
 load_game_button = tk.Button(text="Load Game", command=load_game_function)
 options_button = tk.Button(text="Options", command=options_function)
+exit_button = tk.Button(text="Exit", command=exit_function)
 
 # Plaats knoppen in het venster
-new_game_button.pack(side=tk.TOP)
-load_game_button.pack(side=tk.BOTTOM)
-options_button.pack(side=tk.BOTTOM)
+""""
+plaats achter pack in de () de tekst side=tk.BOTTOM of side=tk.TOP om positie aan te passen
+"""
+new_game_button.pack()
+load_game_button.pack()
+options_button.pack()
+exit_button.pack()
 
 # Stel de grootte van het venster in
 window.geometry("400x200")
